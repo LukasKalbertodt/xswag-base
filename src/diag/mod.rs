323b3c1c120c2d05861347f2559d23d82a712eb7
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-02-23 10:17:14 +0100

    Clear repository for GH-pages

--- src/diag/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Types and functions for error diagnostic and printing
-//!
-
-mod report;
-mod print;
-
-pub use self::report::*;
-pub use self::print::*;
--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! `xswag-base` provides basic functionality for the xswag system.
-//!
-extern crate term_painter;
-
-pub mod code;
-pub mod diag;
